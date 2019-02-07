afc48c09e9e193e1a417587bf375d8b8d734f717
eduinnarelli <37228768+eduinnarelli@users.noreply.github.com>
2019-02-07 19:03:00 -0200

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
+console.log('test');
 
 const jsx = (
   <Provider store={store}>
