cdf557667bcc3144df077c6ae91614e335b6e7c5
eduinnarelli <37228768+eduinnarelli@users.noreply.github.com>
2019-02-08 16:15:11 -0200

    Setup formatting for amount and createdAt

--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@ git status - View the changes to your project code
 git add - Add files to stagind area
 git commit - Creates a new commit with files from stagin area
 git log - View recent commits
+git push - Push commit to github
+git push heroku master - Push commit to heroku
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+import locale from 'numeral/locales';
+
+numeral.locale('pt-br');
 
 const ExpenseListItem = ({id, description, amount, createdAt}) => (
 
@@ -9,7 +14,11 @@ const ExpenseListItem = ({id, description, amount, createdAt}) => (
       <h4>{description}</h4>
     </Link>
 
-    <p> {amount} - {createdAt} </p>
+    <p> 
+      {numeral(amount / 100).format('$0,0.00')}
+      - 
+      {moment(createdAt).format('MMMM Do, YYYY')} 
+    </p>
 
   </div>
 
